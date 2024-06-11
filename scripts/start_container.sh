048dac15f06d1cc5b24c2143386e5c9340055091
KundanAntyakula <168166698+KundanAntyakula@users.noreply.github.com>
2024-06-11 10:04:28 +0530

    commit changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,5 +6,3 @@ docker pull docker pull kundanty/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 kundanty/simple-python-flask-app
-
-echo "Container is running on port 5000"
