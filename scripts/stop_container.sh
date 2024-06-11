47af9a83da5140d9c1368295cec50009ac375724
KundanAntyakula <kundanantyakula@gmail.com>
2024-06-11 11:39:18 +0530

    stop_container

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-<<<<<<< HEAD
-=======
 # Stop the running container (if any)
-containerid = sudo docker ps | awk -F " " '{print $1}'
-
-sudo docker rm -f $containerid
- 
->>>>>>> 67c0f7c (cc)
+containerid=$(sudo docker ps | awk -F " " '{print $1}')
+if [ -n "$containerid" ]; then
+  sudo docker rm -f $containerid
+fi
