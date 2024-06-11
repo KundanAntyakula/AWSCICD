5b4419319821e43ee8ecf71a03c35eaa62e567c8
KundanAntyakula <168166698+KundanAntyakula@users.noreply.github.com>
2024-06-11 12:00:24 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull kundanty/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 8080:80 kundanty/simple-python-flask-app
+docker run -d -p 5000:5000 kundanty/simple-python-flask-app
